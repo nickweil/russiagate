61d53b6b2b14902fe4f4e6915446ae83e2da82b9
nickweil <43078610+nickweil@users.noreply.github.com>
2018-09-13 10:57:11 -0700

    adding html items

---
+++ app.js
@@ -0,0 +1,39 @@
+
+var search = instantsearch({
+  // Replace with your own values
+  appId: 'DTH6V8NWJ2',
+  apiKey: 'fd1f8a02a87cf784faf55a8a686700e7', // search only API key, no ADMIN key
+  indexName: 'records',
+  urlSync: true,
+  searchParameters: {
+    hitsPerPage: 100
+  }
+});
+
+// Add this after the previous JavaScript code
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-input'
+  })
+);
+
+// Add this after the previous JavaScript code
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: document.getElementById('hit-template').innerHTML,
+      empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
+    }
+  })
+);
+
+// Add this after the other search.addWidget() calls
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination'
+  })
+);
+
+// Add this after all the search.addWidget() calls
+search.start();
