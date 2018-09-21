b739f1f869fa4ca3a23baba20094c03d6df0803e
nickweil <43078610+nickweil@users.noreply.github.com>
2018-09-21 10:27:05 -0700

    update

--- app.js
+++ app.js
@@ -64,6 +64,18 @@ search.addWidget(
 // Add this after all the search.addWidget() calls
 search.start();
 
+$(function(){
+  $("#home-button").on({
+   mouseenter: function(){
+    $(this).attr('src','http://www.clker.com/cliparts/d/3/R/P/q/e/hot-pink-home-icon-md.png');
+  },
+  mouseleave: function(){
+    $(this).attr('src','http://openclipart.org/image/800px/svg_to_png/17103/claudita_home_icon.png');
+  }
+  });
+  
+});
+
 // ---------------------
 //
 //  Helper functions
