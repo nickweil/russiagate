14ea2c287757bcdd3e0b6f567261952ee0f70039
nickweil <43078610+nickweil@users.noreply.github.com>
2018-10-08 15:48:47 -0700

    update

--- docs/browse-app.js
+++ docs/browse-app.js
@@ -1,7 +1,1 @@
 
-var element = document.getElementById("box");
-
-element.scrollIntoView();
-element.scrollIntoView(false);
-element.scrollIntoView({block: "end"});
-element.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"});
