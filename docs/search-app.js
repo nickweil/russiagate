d48b0fbe09a61d8a97cf75dfdbcbfa7721dd7fae
nickweil <nick.wheel@gmail.com>
2019-02-12 23:06:09 -0800

    update

--- docs/search-app.js
+++ docs/search-app.js
@@ -1,111 +1,69 @@
-'use strict';
-/* global instantsearch */
 
 var search = instantsearch({
+  // Replace with your own values
   appId: 'DTH6V8NWJ2',
   apiKey: 'fd1f8a02a87cf784faf55a8a686700e7', // search only API key, no ADMIN key
   indexName: 'texts',
-  routing: true,
+  urlSync: true,
   searchParameters: {
-    hitsPerPage: 100
+    hitsPerPage: 200
   }
 });
 
-// SEARCH BOX
+/*texts.setSettings({
+  paginationLimitedTo: 5000
+});*/
+
+// Add this after the previous JavaScript code
 search.addWidget(
   instantsearch.widgets.searchBox({
-    container: '#search-input',
-	placeholder: 'search...'
+    container: '#search-input'
   })
 );
 
-search.on('render', function() {
-  $('.hit-image img').addClass('transparent');
-  $('.hit-image img').one('load', function() {
-      $(this).removeClass('transparent');
-  }).each(function() {
-      if(this.complete) $(this).load();
-  });
-});
-
-var hitTemplate =
-  '<article class="hit" id="{{unixtimestamp}}">' +
-      '<div class="hit-content">' +
-        '<p class="hit-category-breadcrumb">{{{date}}} {{{weekday}}}</p>' +
-      '</div>' +
-      '<div class="hit-sender">' +
-        '<img src="{{senderimgurl}}" alt="{{sender}}">' +
-      '</div>' +
-	  '<div class="hit-image">' +
-        '<img src="{{textimgurl}}" alt="{{date}}">' +
-      '</div>' +
-  '</article>';
-
-var noResultsTemplate =
-  '<div class="text-center">No results found matching <strong>{{query}}</strong>.</div>';
-
-var menuTemplate =
-  '<a href="javascript:void(0);" class="facet-item {{#isRefined}}active{{/isRefined}}"><span class="facet-name"><i class="fa fa-angle-right"></i> {{label}}</span class="facet-name"></a>';
-
-var facetTemplateCheckbox =
-  '<a href="javascript:void(0);" class="facet-item">' +
-    '<input type="checkbox" class="{{cssClasses.checkbox}}" value="{{label}}" {{#isRefined}}checked{{/isRefined}} />{{label}}' +
-    '<span class="facet-count">({{count}})</span>' +
-  '</a>';
-
-var facetTemplateColors =
-  '<a href="javascript:void(0);" data-facet-value="{{label}}" class="facet-color {{#isRefined}}checked{{/isRefined}}"></a>';
-
-// HITS
+// Add this after the previous JavaScript code
 search.addWidget(
   instantsearch.widgets.hits({
     container: '#hits',
-	hitsPerPage: 100,
     templates: {
-      item: hitTemplate,
-      empty: noResultsTemplate
+      item: document.getElementById('hit-template').innerHTML,
+      empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
     }
   })
 );
 
-// BATCH REFINEMENT  
 search.addWidget(
     instantsearch.widgets.refinementList({
       container: '#batch-refinement',
       attributeName: 'batch',
       templates: {
-        header: 'Batch',
-		item: menuTemplate
+        header: 'Batch'
       }
     })
   );
   
-// SENDER REFINEMENT  
-search.addWidget(
+  search.addWidget(
     instantsearch.widgets.refinementList({
       container: '#sender-refinement',
       attributeName: 'sender',
       templates: {
-        header: 'Sender',
-		item: menuTemplate
+        header: 'Sender'
       }
     })
   );
-
-// WEEKDAY REFINEMENT  
-search.addWidget(
+  
+    search.addWidget(
     instantsearch.widgets.refinementList({
       container: '#weekday-refinement',
       attributeName: 'weekday',
       templates: {
-        header: 'Day of the week',
-		item: menuTemplate
+        header: 'Day of the week'
       }
     })
   );
 
 
-// PAGINATION
+// Add this after the other search.addWidget() calls
 search.addWidget(
   instantsearch.widgets.pagination({
     container: '#pagination',
@@ -117,3 +75,36 @@ search.addWidget(
 
 // Add this after all the search.addWidget() calls
 search.start();
+
+
+
+// ---------------------
+//
+//  Helper functions
+//
+// ---------------------
+function getTemplate(templateName) {
+  return document.querySelector(`#${templateName}-template`).innerHTML;
+}
+
+function getHeader(title) {
+  return `<h5>${title}</h5>`;
+}
+
+function getCategoryBreadcrumb(item) {
+  const highlightValues = item._highlightResult.categories || [];
+  return highlightValues.map(category => category.value).join(' > ');
+}
+
+function getStarsHTML(rating, maxRating) {
+  let html = '';
+  const newRating = maxRating || 5;
+
+  for (let i = 0; i < newRating; ++i) {
+    html += `<span class="ais-star-rating--star${
+      i < rating ? '' : '__empty'
+    }"></span>`;
+  }
+
+  return html;
+}
