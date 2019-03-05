d1329d92d34ae5f6f40aae12a94e5730581a643f
nickweil <nick.wheel@gmail.com>
2019-03-04 22:41:39 -0800

    update

--- docs/search-app.js
+++ docs/search-app.js
@@ -4,7 +4,7 @@ var search = instantsearch({
   appId: 'DTH6V8NWJ2',
   apiKey: 'fd1f8a02a87cf784faf55a8a686700e7', // search only API key, no ADMIN key
   indexName: 'texts',
-  urlSync: true,
+  routing: true,
   searchParameters: {
     hitsPerPage: 200
   }
